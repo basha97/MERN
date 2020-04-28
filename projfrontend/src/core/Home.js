cc0239f5a7147b90b7ed7a3db2451bea9c6f4ce3
Adil Basha <bashaadil2727@gmail.com>
2020-04-28 14:47:20 +0530

    Adding Bootstrap and css to frontend

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import "../styles.css";
 
 function Home(props) {
     return (
         <div>
-          <h1>Hello front end</h1>  
+          <h1 className="text-white">Hello front end</h1>  
         </div>
     );
 }
