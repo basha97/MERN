caa42f7c142d5c258928eb579c7acc3df1a9929a
Adil Basha <bashaadil2727@gmail.com>
2020-04-28 16:17:41 +0530

    Adding base component

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import "../styles.css";
+import { API } from "../backend";
+import Base from './Base';
 
-function Home(props) {
+function Home() {
+  console.log('Api is', API);
     return (
-        <div>
-          <h1 className="text-white">Hello front end</h1>  
-        </div>
+        <Base title="Home Page">
+          <div className="row">
+            <div className="col-4">
+              <button className="btn btn-success">Test</button>
+            </div>
+            <div className="col-4"></div>
+            <div className="col-4"></div>
+          </div> 
+        </Base>
     );
 }
 
