358776a8029e457ebe6f1223273b1f1127d679f4
Adil Basha <bashaadil2727@gmail.com>
2020-04-29 13:28:08 +0530

    Testing signup form

--- projfrontend/src/core/Base.js
+++ projfrontend/src/core/Base.js
@@ -12,9 +12,9 @@ const Base = ({
         <div className="container-fluid">
             <div className="jumbotron bg-dark text-white text-center">
                 <h2 className="display-4">{title}</h2>
-                <p className="lead">{description}</p>
+                <p className="lead" >{description}</p>
             </div>
-            <div className={className}>{children}</div>
+            <div className={className} >{children}</div>
         </div> 
         <footer className="footer bg-dark mt-auto py-3">
             <div className="container-fluid bg-success text-white text-center py-3">
